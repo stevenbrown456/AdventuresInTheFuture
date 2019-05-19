793cc5e94c73a3aea5ae06ca02a908655f8c20d2
stevenbrown456 <43067271+stevenbrown456@users.noreply.github.com>
2019-05-19 22:10:59 +0100

    added in platforms and character movement
    Added in plattforms and character movement within the game

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+    //These are the desinger variables
+    public float speed = 10;
+    public float jumpSpeed = 10;
+    public Rigidbody2D physicsBody;
+    public string horizontalAxis = "Horizontal";
+
+    public SpriteRenderer playerSprite;
+    public Collider2D playerCollider;
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Jump();
+        // Get axis input from Unity
+        float leftRight = Input.GetAxis(horizontalAxis);
+
+        // Create direction vector from axis input
+        Vector2 direction = new Vector2(leftRight, 0);
+
+        // Make direction vector length 1
+        direction = direction.normalized;
+
+        // Calculate velocity
+        Vector2 velocity = direction * speed;
+        velocity.y = physicsBody.velocity.y;
+
+        // Give the velocity to the rigidbody
+        physicsBody.velocity = velocity;
+
+        
+       }
+    
+    void Jump()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, 5f), ForceMode2D.Impulse);
+        }
+
+    }
+}
